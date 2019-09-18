ec6484ef0dde685a736e73d05cf07d3e3f5a14ac
Jason Thai <jasonnthaii@gmail.com>
2019-09-18 12:07:11 -0700

    Basic player scripts + scaling title screen for different aspect ratios

---
+++ RNGesus/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class BaseEnemy
+{
+    public string name;
+
+    public float baseHP;
+    public float currentHP;
+
+    public float baseMP;
+    public float currentMP;
+
+    int strenth;
+    int magic;
+    int dexterity;
+    int agility;
+}
