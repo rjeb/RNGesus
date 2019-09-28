7e11969a6e7d9dd8ef8aeff5e6d0789ae8795280
PhongTrinh7 <42724318+PhongTrinh7@users.noreply.github.com>
2019-09-28 13:21:23 -0700

    overworld

--- RNGesus/Assets/Scripts/OverworldHandler.cs
+++ RNGesus/Assets/Scripts/OverworldHandler.cs
@@ -4,6 +4,76 @@ using UnityEngine;
 
 public class OverworldHandler : MonoBehaviour
 {
+
+    private class MapTree
+    {
+        private MapNode firstLevel;
+
+        public MapTree()
+        {
+            firstLevel = new MapNode();
+        }
+
+        public MapNode newMap(GameObject button, Vector3 position, MapNode pred)
+        {
+            newMap = new MapNode(button, position, pred);
+        }
+        
+        private class MapNode
+        {
+            private GameObject sprite;
+            private Vector3 position;
+            private bool discovered, accessible;
+            private MapNode predecessor;
+
+            private MapNode()
+            {
+                position = new Vector3(0, 0, 0);
+                discovered = true;
+                reachable - true;
+            }
+
+            private MapNode(GameObject sprite, Vector3 position, MapNode pred)
+            {
+                discovered = false;
+                reachable = false;
+            }
+
+            private MapNode getNext()
+            {
+                return nextNode;
+            }
+
+            private void setNext(MapNode next)
+            {
+                nextNode = next;
+            }
+
+            private bool discovered()
+            {
+                return discovered;
+            }
+
+            private bool access()
+            {
+                return accessible;
+            }
+
+            private void makeAvailable()
+            {
+                discovered = true;
+                accessible = false;
+            }
+
+            private void clear()
+            {
+                accessible = false;
+            }
+        }
+    }
+
+
+
     public GameObject mapNode;
     // Start is called before the first frame update
     void Start()
