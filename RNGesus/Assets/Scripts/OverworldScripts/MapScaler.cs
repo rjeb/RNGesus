818374a79bbbe099adf72a99a87a3d94a25d05cb
Jason Thai <jasonnthaii@gmail.com>
2019-11-18 13:04:17 -0800

    MapScaler Bugfix

--- RNGesus/Assets/Scripts/OverworldScripts/MapScaler.cs
+++ RNGesus/Assets/Scripts/OverworldScripts/MapScaler.cs
@@ -12,9 +12,12 @@ public class MapScaler : MonoBehaviour
         float worldScreenHeight = Camera.main.orthographicSize * 2;
         float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width + (float) .1;
 
-        transform.localScale = new Vector3(
+        if (sr != null)
+        {
+            transform.localScale = new Vector3(
             worldScreenWidth / sr.sprite.bounds.size.x,
             worldScreenHeight / sr.sprite.bounds.size.y, 1);
+        }
 
     }
 
